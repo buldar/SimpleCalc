c34a14305c9df3a3656515a68ea4f2618357fd2e
Buldar <buldar666@gmail.com>
2022-05-25 17:28:39 +0300

    some css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div className="App">
+            <div className='Tittle'><h2>I AM CALCULATOR</h2></div>
+            <div className='Calc'>
+                <div className='Display'><span>123</span></div>
+                <div className='Sets'>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                </div>
+                <div className='Functional'>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <div>
+                        <button style={{height: '130px'}}>+</button>
+                    </div>
+                    <Button/>
+                    <Button/>
+                    <Button/>
+                    <div>
+                        <button style={{height: '130px'}}>=</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
-export default App;
+export const Button = () => {
+    return (
+        <div>
+            <button>1</button>
+        </div>
+    )
+}
+
